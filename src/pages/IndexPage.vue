0b198700ae8864848a70b63042a0690cc1d6ac59
Gregor Twickler <gregor@twickler.de>
2025-11-24 22:57:40 +0100

    FirstCommit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
-  </q-page>
+  <div class="flex row justify-center">
+  <div class="flex column justify-center justify-start text-center" style="background-color: white;">
+    <h4>e-Bike Welt Havixbeck</h4>
+    <h5>Hochwertige e-Bikes und Meisterwerkstatt im Münsterland</h5>
+    <text-body1>
+      Wir sind spezialisiert auf hochwertige e-Bikes, bieten zu unseren Premium Marken wie Riese und Müller, Specialized, Van Raam und Urban Arrow aber auch günstige Einstiegsmodelle und Leasingrückläufer und wir garantieren darüber hinaus auch noch sehr attraktive Preis für alle Modelle mit der Bestpreis Garantie auch mit Leasing und 0% Finanzierung! > Mehr über uns
+    </text-body1>
+  </div>
+  <div class="flex column justify-center justify-start text-center" style="background-color: green;">
+    <h4>Meisterwerkstatt im Münsterland</h4>
+    <q-card style="background-color:green;">
+      <q-card-section class="my-card q-pt-none" style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+        <div style="position:relative; top:-30px; left:-15px; background:white">
+        <q-icon name="warning"></q-icon>
+        </div>
+        <h6>Werkstatt Service</h6>
+        <text-body1>
+          Neben der Neurad-Montage und Inspektionen für dein e-Bike bieten wir alle Art von Reparaturen an Fahrrädern und e-Bikes auch für v...
+        </text-body1>
+      </q-card-section>
+    </q-card>
+  </div>
+  </div>
 </template>
 
 <script setup>
 //
 </script>
+
+<style lang="scss" scoped>
+  .my-card{
+    width :250px;
+    padding: 30px 40px;
+    margin-left:30px;
+    margin-bottom:30px;
+  }
+    
+</style>
